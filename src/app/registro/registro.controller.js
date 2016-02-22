684e837fb9beddc953839fae59a6c1c265bf254b
sbalazote <sbalazote@gmail.com>
2016-02-22 00:08:21 -0300

    Fix de html respecto al servicio de registracion

--- src/app/registro/registro.controller.js
+++ src/app/registro/registro.controller.js
@@ -12,15 +12,14 @@
 
         function RegistroController($scope,$location,Registration) {
       		$scope.form = {
-        			name: '',
-              lastName: '',
-              userName: '',
+              username: '',
+              email: '',
               password: '',
-              confirmPassword: '',
+              nombreYApellido: '',
               dni: '',
-              phoneNumber: '',
+              telefono: '',
               role: ''
-          }
+          };
           
           $scope.error = '';
 
@@ -32,19 +31,20 @@
           });
 
           var isOk = function() {
-          if($scope.form.name.length !== 0 && $scope.form.lastName.length !== 0 && $scope.form.userName.length !== 0 && $scope.form.password.length !== 0 && $scope.form.confirmPassword.length !== 0 && $scope.form.dni.length !== 0 && $scope.form.phoneNumber !== 0 && $scope.form.password == $scope.form.confirmPassword)
-            return true;
-          else
-            return false;
+            if($scope.form.userName.length !== 0 && $scope.form.email.length && $scope.form.password.length !== 0 && $scope.form.nombreYApellido.length !== 0 && $scope.form.dni.length !== 0 && $scope.form.telefono !== 0) {
+                return true;
+            } else{
+              return false;
+            }
           };
-
+ 
           $scope.backToLogin = function() {
             $location.url('/login');
           };
 
           //TODO: Submit Button disable at the begining, and then enable it
           //TODO: Validate the confirm password at writing moment
-          
+          //TODO: Confirm Password and compare with the original
           //TODO: Make all the form smaller
           //TODO: FIX delay to hide the hint message and hide it when you have maxlength
         
@@ -54,13 +54,13 @@
 
               Registration.register($scope.form,
                 
-                function(data) {
+                function() {
                   $scope.isAvailable = false;
                   $scope.errorFlag = false;
-                  $scope.message = 'Registration was Successfully'
+                  $scope.message = 'Registración de Usuario Exitosa';
                 },
                 function() {
-                  $scope.error = 'Unable to Register a New User';
+                  $scope.error = 'No es Posible Registrar un Nuevo Usuario';
                   $scope.errorFlag = true;
                 }
               );

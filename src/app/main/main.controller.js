4dcd9599c2ea34efdfd2d9d10bcf8e2b258c7b34
javiercelta <ja.rodriguez.vazquez@gmail.com>
2016-03-10 16:34:49 -0300

    Formulario alta de paciente

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -3,26 +3,28 @@
 
     angular
     	.module('myApp')
-      	.controller('MainCtrl', MainController);
+      .controller('MainCtrl', MainController);
         
-      	MainController.$inject = ['$scope', '$mdDialog', '$mdMedia', 'Paciente'];
+      MainController.$inject = ['$scope', '$mdDialog', '$mdMedia', 'Paciente'];
 
-      	function MainController($scope, $mdDialog, $mdMedia, Paciente) {
-      		$scope.contentUrl = '';
+      function MainController($scope, $mdDialog, $mdMedia, Paciente) {
+      	$scope.contentUrl = '';
 
-      		$scope.altaPaciente = {
-                dni: '23.123.681',
-                nombreYApellido: 'javier',
-                direccion: 'fdff',
-                telefono: '4578-8652',
-                antecedentesFamiliares: 'No tiene',
-                observaciones:'ninguna'
-            };
+      	$scope.altaPaciente = {
+          dni: '',
+          nombreYApellido: '',
+          direccion: '',
+          telefono: '',
+          antecedentesFamiliares: '',
+          observaciones:''
+        };
 
-      		$scope.showBusquedaPacienteModal = function(ev) {
+        $scope.altaPacienteError = '';
+
+      	$scope.showBusquedaPacienteModal = function(ev) {
     			var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
 		    	$mdDialog.show({
-		      		controller: DialogController,
+		      	controller: DialogController,
 				    templateUrl: 'app/main/busquedaPaciente.tmpl.html',
 				    parent: angular.element(document.body),
 				    targetEvent: ev,
@@ -31,8 +33,9 @@
 		    	})
     			.then(function(answer) {
       			}, function() {
-      			});
-			    $scope.$watch(function() {
+      		});
+			    
+          $scope.$watch(function() {
 			      return $mdMedia('xs') || $mdMedia('sm');
 			    }, function(wantsFullScreen) {
 			      $scope.customFullscreen = (wantsFullScreen === true);
@@ -43,28 +46,28 @@
     					$mdDialog.hide();
   					};
 			
-					$scope.cancel = function() {
+					  $scope.cancel = function() {
 			    		$mdDialog.cancel();
 			  		};
 			  	
 			  		$scope.answer = function(answer) {
 			    		$mdDialog.hide(answer);
 			  		};
-				}
-			};
+				  }
+			  };
 
-			$scope.changeView = function (view) {
-    			$scope.contentUrl = 'app/main/' + view + '.tmpl.html';
-			}
+			  $scope.changeView = function (view) {
+    		  $scope.contentUrl = 'app/main/' + view + '.tmpl.html';
+			  }
 
-			$scope.crearPaciente = function () {
-                Paciente.altaPaciente($scope.altaPaciente,
-                    function(data) {
-                    },
-                    function() {
-                    }
-                );
-            };
-
-      	}
+			  $scope.crearPaciente = function () {
+          Paciente.altaPaciente($scope.altaPaciente,
+            function(data) {
+            },
+            function() {
+              $scope.altaPacienteError = 'El paciente no pudo ser dado de alta';
+            }
+          );
+        };
+      }
 })();
